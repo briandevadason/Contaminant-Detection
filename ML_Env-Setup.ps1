ae95a23a540f023e4350c67ad4931cafab22a0df
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-07 15:52:56 -0500

    Update ML_Env-Setup.ps1

--- ML_Env-Setup.ps1
+++ ML_Env-Setup.ps1
@@ -6,5 +6,20 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
   exit
 }
 
-wsl --install 
-shutdown.exe /r /t 10
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+
+wsl --set-default-version 2
+
+Restart-Computer
+
+# Enable the VirtualMachinePlatform feature
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+
+# Set the WSL version to 2
+wsl --set-default-version 2
+
+# Restart the computer
+Restart-Computer
+
+#wsl --install 
+#shutdown.exe /r /t 10
