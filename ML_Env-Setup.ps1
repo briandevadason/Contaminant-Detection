d3e9562af1bd878cb0d010e52f50fca24b74bb71
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-07 16:34:37 -0500

    Update ML_Env-Setup.ps1

--- ML_Env-Setup.ps1
+++ ML_Env-Setup.ps1
@@ -6,18 +6,18 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
   exit
 }
 
+# Start the WSL --install process
+Invoke-Expression "wsl --install"
+
+
+# Wait for the process to complete
+Wait-Process -Id $process.Id
 
 "Enabling Virtual Machine Platform feature"
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 "Enabling WSL feature"
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-
-# Start the WSL --install process
-Invoke-Expression "wsl --install"
-
-# Wait for the process to complete
-Wait-Process -Id $process.Id
 
 # Restart the computer
 Restart-Computer -Force
