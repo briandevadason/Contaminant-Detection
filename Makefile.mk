b32de7cfe92dead4306ec1abb0506d76fed398ea
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-07 18:10:26 -0500

    Update Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -1,24 +1,16 @@
-install:
-    # Download Python 3.8 source code
-    wget https://www.python.org/ftp/python/3.8.6/Python-3.8.6.tgz
+# This is the default target
+all: install_python install_conda
 
-    # Extract tarball
-    tar xvf Python-3.8.6.tgz
+install_python:
+	# Download and install Python 3.8
+	wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tar.xz
+	tar xvf Python-3.8.5.tar.xz
+	cd Python-3.8.5 && ./configure --prefix=/usr/local && make && make install
 
-    # Navigate to extracted directory
-    cd Python-3.8.6
-
-    # Run configure script
-    ./configure --enable-optimizations
-
-    # Compile and install Python
-    make -j8
-    sudo make altinstall
-
-    # Add conda to PATH
-    echo 'export PATH="/opt/conda/bin:$PATH"' >> ~/.bashrc
-    source ~/.bashrc
-
-    # Update conda and install packages
-    conda update -n base -c defaults conda
-    conda install numpy scipy pandas
+install_conda:
+	# Download and install Miniconda, a lightweight version of Anaconda
+	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+	bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+	# Add Miniconda to the PATH
+	echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+	source ~/.bashrc
