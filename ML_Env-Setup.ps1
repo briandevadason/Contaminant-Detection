927d8ee3e7f3294e564ef48e58fd36359d8e181b
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-07 15:41:43 -0500

    Create ML_Env-Setup.ps1

---
+++ ML_Env-Setup.ps1
@@ -0,0 +1,10 @@
+set-executionpolicy -scope CurrentUser -executionPolicy Bypass -Force
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+  exit
+}
+
+wsl --install 
+shutdown.exe /r /t 10
