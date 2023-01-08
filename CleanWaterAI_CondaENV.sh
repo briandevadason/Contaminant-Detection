133ec62b005fcb99968ae9f97a450978eccfca96
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-08 00:00:00 -0500

    Update CleanWaterAI_CondaENV.sh

--- CleanWaterAI_CondaENV.sh
+++ CleanWaterAI_CondaENV.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Download the Miniconda installer
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+# Run the Miniconda installer
+bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+
+# Add Miniconda to the PATH
+echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
 # Update conda
 conda update -n base -c defaults conda
 
