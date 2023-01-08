68ed2fba74527b82aaff14c7adeada5dc933ccd4
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-08 01:13:15 -0500

    Update Makefile.mk

--- Makefile.mk
+++ Makefile.mk
@@ -1,12 +1,9 @@
-# This is the default target
-all: install_conda
-
-install_conda:
-	# Download and install Miniconda, a lightweight version of Anaconda
-	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-	bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
-	# Add Miniconda to the PATH
-	echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
-	source ~/.bashrc
-# Add install_conda env to the script
+install:
+        # Download the Miniconda installer
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+        # Run the Miniconda installer
+        bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+        # Add Miniconda to the PATH
+        echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+        source ~/.bashrc
 
