3e0d8a1605736747f202d09dc6c1cbaaec27a316
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-08 00:25:30 -0500

    Update Conda_Installer.sh

--- Conda_Installer.sh
+++ Conda_Installer.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Download Python 3.8
+wget https://www.python.org/ftp/python/3.8.5/Python-3.8.5.tar.xz
+
+# Extract the tarball
+tar xvf Python-3.8.5.tar.xz
+
+# Change to the Python 3.8 directory
+cd Python-3.8.5
+
+# Configure, build, and install Python
+./configure --prefix=/usr/local && make && make install
+
 # Download the Miniconda installer
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
