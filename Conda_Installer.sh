6ffec116944bac7510413777498199bb654b8765
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-08 01:09:09 -0500

    Update Conda_Installer.sh

--- Conda_Installer.sh
+++ Conda_Installer.sh
@@ -12,15 +12,6 @@ apt install -y python3.8
 #Install Make
 apt install -y make
 
-#Miniconda download
-wget https://repo.anaconda.com/miniconda/Miniconda3-py38_22.11.1-1-Linux-x86_64.sh
+#Run the make file to install Conda
+sudo make -f Makefile.mk full
 
-# Make the Miniconda installer executable
-chmod +x Miniconda3-py38_22.11.1-1-Linux-x86_64.sh
-
-# Run the Miniconda installer
-./Miniconda3-py38_22.11.1-1-Linux-x86_64.sh -b -p $HOME/miniconda
-
-# Add Miniconda to the PATH
-echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
