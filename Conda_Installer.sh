03706c37711b11dcdd1cd895c388227224128ad4
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-08 00:57:30 -0500

    Update Conda_Installer.sh

--- Conda_Installer.sh
+++ Conda_Installer.sh
@@ -12,11 +12,15 @@ apt install -y python3.8
 #Install Make
 apt install -y make
 
-# Clone the conda repository
-git clone https://github.com/conda/conda.git
+#Miniconda download
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_22.11.1-1-Linux-x86_64.sh
 
-# Change to the conda directory
-cd conda
+# Make the Miniconda installer executable
+chmod +x Miniconda3-py38_22.11.1-1-Linux-x86_64.sh
 
-# Run the install script
-bash install.sh
+# Run the Miniconda installer
+./Miniconda3-py38_22.11.1-1-Linux-x86_64.sh -b -p $HOME/miniconda
+
+# Add Miniconda to the PATH
+echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
