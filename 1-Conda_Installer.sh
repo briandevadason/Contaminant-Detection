dd0ec42f81f2d59e491490ac6cd714d7efbcd94d
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-20 23:48:38 -0500

    Update 1-Conda_Installer.sh

--- 1-Conda_Installer.sh
+++ 1-Conda_Installer.sh
@@ -25,5 +25,9 @@ chmod +x Anaconda3-2022.10-Linux-x86_64.sh
 
 bash Anaconda3-2022.10-Linux-x86_64.sh
 
-echo 'export PATH="/miniconda3/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="/home/ubuntu/anaconda3/bin/conda"' >> ~/.bashrc
 source ~/.bashrc
+
+
+#echo 'export PATH="/miniconda3/bin:$PATH"' >> ~/.bashrc
+#source ~/.bashrc
