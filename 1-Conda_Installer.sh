bb4db41f7cb2995a31592c8721102c11ca145fc8
briandevadason <115530066+briandevadason@users.noreply.github.com>
2023-01-21 19:20:45 -0500

    Update 1-Conda_Installer.sh

--- 1-Conda_Installer.sh
+++ 1-Conda_Installer.sh
@@ -11,6 +11,9 @@ apt upgrade -y
 # Install Python 3.8 without user interaction
 apt install -y python3
 
+#Anaconda prerequsites
+apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+
 # pip install
 apt install -y python3-pip
 
